dfb9b6a8496160369290115f8bbec140579258d2
Tarush Gupta <tgupta22@uic.edu>
2019-12-04 02:44:49 +0000

    Made a lot of changes, CHANGED THE BUS TRACKER APP TO SHOW YOUR FAVOURAITE BUS SCHEDULE. Also added a camera and box to added a picture with your favouraite bus stop

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let ctaBuses =  {
-  "1": {"code":"1", "title":"Bus Number 1 (Bronzeville/Union Station)"},
+  "1": {"code":"1", "title":"Bus Number 1 (Monday thru Friday 1 Bronzeville/Union Station Northbound - Southbound => 35th/Indiana, 16th/Michigan, Van Buren/Michigan, Adams/Canal, Union Station) "},
+  "2": {"code":"2","title":"Bus Number 2 (Hyde Park Express, Northbound -> SouthBound => Cottage Grove, Bowen, Hyde Park, Harrison, Ohio, Navy Pier)"},
+  "3": {"code":"3", "title": "Bus Number 3 (King Drive, Northbound -> SouthBound => Cottage Grove, King Dr, 63rd Green Line, 35th, Cenmark, Wacker, Fairbanks)"},
+  "4": {"code":"4", "title": "Bus Number 4 (Cottage Grove, Northbound -> SouthBound => 115th, 95th/St, Cottage Grove, 63rd, King Drive, Michigan, South Water )"},
+  "5": {"code":"5", "title": "Bus Number 5(South Shore Night Bus, Northbound -> SouthBound => Red line, Jeffery, South Shore, Commercial, Stony Island, 95th Red Line)"},
+  "6":{"code":"6","title":"Bus Number 6 (Jackson Park Express, Northbound -> SouthBound => South Shore, Stony Island, 57th/ Stony Island, Lake Park, Columbus, Wacker)"},
+  "7": {"code":"7", "title":"Bus Number 7 (Harrison , Northbound -> SouthBound => Harrison, Pulaski, Kedzie, Racine, Jackonson, Congress Plaza)"},
+  "8": {"code":"8", "title": "Bus Number 8 (Halsted, Northbound -> SouthBound => Halsted, Root, Orange Line Station, UIC Halstead, North Branch, Fullerton, Waveland )"},
+
+  "60": {"code":"60", "title": "Bus Number 60 (Blue Island/26th, Northbound -> SouthBound => Cicero, Kedzie, Ashland, Harrison/Halstead, Washington, Harbor Drive)"},
+  "J14":{"code":"J14","title":"Bus Number J14 (Jeffery Jump, Northbound -> SouthBound => Stony Island, Jeffery, 79th, Jeffery, Wabash, Washington,  )"},
+  "X49": {"code":"X49", "title":"Bus Number X49 (Western Express, Northbound -> SouthBound => 79th Terminal, Western Orange Line, Archer, Harrison Blue Line, Western, Berwyn)"},
+  "111": {"code":"111", "title": "Bus Number 111(111th/King Drive, Northbound -> SouthBound => Marshfield, Vincennes, Halstead, Michigan, King, 95th Red Line)"}
+
+
+}
+
+
+let ctaBusStops = 
+    {
+
+      "1": {"code":"1", "title":"Bus Number 1 (Bronzeville/Union Station)"},
   "2": {"code":"2","title":"Bus Number 2 (Hyde Park Express)"},
   "3": {"code":"3", "title": "Bus Number 3 (King Drive)"},
   "4": {"code":"4", "title": "Bus Number 4 (Cottage Grove)"},
@@ -13,5 +34,7 @@ let ctaBuses =  {
   "X49": {"code":"X49", "title":"Bus Number X49 (Western Express)"},
   "111": {"code":"111", "title": "Bus Number 111(111th/King Drive)"}
 
-
-}
+    
+    
+    
+    }
